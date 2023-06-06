5dc40a39e03b1f733c3fdb78aa02cb59a72da829
MauriRodiez <c.maurodriguez.a@gmail.com>
2023-06-05 22:24:57 -0300

    subiendo ejercicios de repaso

---
+++ ejercicioRepaso.js
@@ -0,0 +1,13 @@
+/* 
+1)Las personas encargadas del backend nos enviaron una lista de productos pero 
+    se olvidaron de asignarles un ID unico para cada producto,
+    nuestra tarea es agregarles ese ID unico a cada uno
+    Requisito: debe empezar en 1 y ser secuencial 
+2) Nos solicitan crear un algoritmo que pueda determinar el dinero total que ingresaria
+    si se venden todos los productos que tenemos en stock
+3) Debemos crear una funcion que al ejecutarse filtre los productos que sean
+    X categoria y retornar dichos productos en un nuevo arreglo(debe ser dinamica)
+4) Por ultimo debemos crear una funcion para poder aumentar el precio de una categoria
+    especifica que recibiremos por parametros y tambien recibiremos por cuanto queremos
+    multiplicar dicho precio
+*/
