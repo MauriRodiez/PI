c13314c418354f69c478d007167fd1ab38698b3f
Maurirodiez <c.maurodriguez.a@gmail.com>
2023-06-08 16:18:31 -0300

    Nuevos ejercicios

--- ejercicioRepaso.js
+++ ejercicioRepaso.js
@@ -312,3 +312,126 @@ console.log(librosEncontrados);
 const edades = [11,12,15,18,25,22,10,33,18,5]
 
 // a) Obtener en un nuevo array las edades menores a 18 
+// b) Obtener en un nuevo array las edades menores a 18.
+// c) Obtener en un nuevo array las edades mayor o igual a 18.
+// d) Obtener en un nuevo array las edades igual a 18.
+// e) Obtener el menor.
+// f) Obtener el mayor. 
+// g) Obtener el promedio de edades.
+// h) Incrementar en 1 todas las edades.
+
+// Ejercicio 2
+
+const arrayCuentas =
+[
+    {
+      titular: "Arlene Barr",
+      estaHabilitada: false,
+      saldo: 3253.40,
+      edadTitular: 33,
+      tipoCuenta: "sueldo"
+    },
+    {
+      titular: "Roslyn Torres",
+      estaHabilitada: false,
+      saldo: 3229.45,
+      edadTitular: 27,
+      tipoCuenta: "sueldo"
+    },
+    {
+      titular: "Cleo Lopez",
+      estaHabilitada: true,
+      saldo: 1360.19,
+      edadTitular: 34,
+      tipoCuenta: "corriente"
+    },
+    {
+      titular: "Daniel Malone",
+      estaHabilitada: true,
+      saldo: 3627.12,
+      edadTitular: 30,
+      tipoCuenta: "sueldo"
+    },
+    {
+        titular: "Ethel Leon",
+        estaHabilitada: true,
+        saldo: 1616.52,
+        edadTitular: 34,
+        tipoCuenta: "sueldo"
+      },
+      {
+        titular: "Harding Mitchell",
+        estaHabilitada: true,
+        saldo: 1408.68,
+        edadTitular: 25,
+        tipoCuenta: "corriente"
+      }
+    ]
+
+
+// a- Obtener un nuevo array de cuentas cuyas edades sean menores a 30.
+
+const filtrarObjetoEdad = array => {
+    let resultado = []
+    for (let i = 0; i < array.length; i++) {
+        
+        if (array[i].edadTitular < 30) {
+            resultado.push(array[i])
+        }        
+    }
+    return resultado
+}
+
+let objetoFiltado = filtrarObjetoEdad(arrayCuentas)
+console.log(objetoFiltado);
+
+// b- Obtener un nuevo array de cuentas cuyas edades sean mayor o igual a 30.
+// c- Obtener un nuevo array de cuentas cuyas edades sean menores o igual a 30.
+// d- Obtener la cuenta con el titular de la misma más joven.
+// e- Obtener un array con las cuentas habilitadas.
+// f- Obtener un array con las cuentas deshabilitadas.
+// g- Obtener la cuenta con el menor saldo.
+// h- Obtener la cuenta con el mayor saldo.
+
+
+// EXTRAS
+
+// 1 - Desarrollar una función llamada generarID que reciba como parámetro el array de cuentas y agregue a cada elemento 
+// (objeto literal) una propiedad llamada id con un valor Numérico
+
+const generarID = array => {
+    
+    for (let i = 0; i < array.length; i++) {  
+        array[i].id = i + 1            
+    }
+    return array
+}
+
+generarID(arrayCuentas)
+console.log(arrayCuentas);
+
+// 2 - Desarrollar una función llamada buscarPorId que reciba como parámetro el array de cuentas y un id, en caso de encontrar 
+// retornar la cuenta (él objeto literal completo), caso contrario retornar null
+
+const buscarXid = (array, num) => {
+
+    for (let i = 0; i < array.length; i++) {
+        if(array[i].id === num){
+            return array[i]
+        }   
+    }
+    return null
+}
+
+let busqueda = buscarXid(arrayCuentas, 2)
+console.log(busqueda);
+
+// 3 - Desarrollar una función llamada filtrarPorSaldos que reciba como parámetro el array de cuentas y un saldo (Number), deberá 
+// retornar un array que se cuyas cuentas se encuentren por debajo del saldo enviado por parámetro
+
+
+
+
+// 4 - Desarrollar una función llamada incrementarSaldo que reciba como parámetro el array de cuentas, un id y un saldo, deberá 
+// incrementar él saldo de la cuenta, en caso de encontrar, caso contrario retornar undefined 
+// Reutilizando la función  buscarPorId 
