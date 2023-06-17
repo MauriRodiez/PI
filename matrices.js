4c3522e673f0e3c137dad330f5691699b10ac9d8
MauriRodiez <c.maurodriguez.a@gmail.com>
2023-06-17 08:07:38 -0300

    Actualizando ejercicio de Matrices

--- matrices.js
+++ matrices.js
@@ -1,26 +1,23 @@
-
-
 /**
  * Declara una variable que contenga una matriz
  * de 5x5 llena de puros numeros enteros y positivos
  */
 
 const llenarMatriz = () => {
-    let matriz = []
-    let num = 0
-    for (let i = 0; i < 5; i++) {
-            matriz[i] = []
-            for (let j = 0; j < 5; j++) {
-                matriz[i][j] = Math.floor(Math.random() * 10)                
-            }    
+  let matriz = [];
+  let num = 0;
+  for (let i = 0; i < 5; i++) {
+    matriz[i] = [];
+    for (let j = 0; j < 5; j++) {
+      matriz[i][j] = Math.floor(Math.random() * 10);
     }
-    return matriz
-}
+  }
+  return matriz;
+};
 
-let matrizCompleta = llenarMatriz()
+let matrizCompleta = llenarMatriz();
 //console.log(matrizCompleta);
 //console.table(matrizCompleta);
-
 
 /**
  * Luego, escribe un algoritmo para sumar todos
@@ -28,39 +25,35 @@ let matrizCompleta = llenarMatriz()
  */
 
 const sumaMatriz = (matrix) => {
-
-    let comodin = 0
-    for (let i = 0; i < matrix.length; i++) {
-            for (let j = 0; j < matrix[i].length; j++) {
-                comodin += matrix[i][j]         
-            }
+  let comodin = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      comodin += matrix[i][j];
     }
-    return comodin
-}
+  }
+  return comodin;
+};
 
-let resultadoSuma = sumaMatriz(llenarMatriz())
-//console.log(resultadoSuma);   
-
-
+let resultadoSuma = sumaMatriz(llenarMatriz());
+//console.log(resultadoSuma);
 
 // Cear una funcion que genere una matiz de 10x10. Cada fila debe tener solo 10 numeros.
 
-const matrix10 = (x,y) =>{
+const matrix10 = (x, y) => {
+  let acc = 1;
+  let matriz = [];
 
-    let acc = 1
-    let matriz = []
-
-    for (let i = 0; i < x; i++) {        
-        matriz[i] = []
-        for (let j = 0; j < y; j++) {
-            matriz[i][j] = acc
-            acc++
-        }
+  for (let i = 0; i < x; i++) {
+    matriz[i] = [];
+    for (let j = 0; j < y; j++) {
+      matriz[i][j] = acc;
+      acc++;
     }
-    return matriz
-}
+  }
+  return matriz;
+};
 
-let resultado = matrix10(10,10)
+let resultado = matrix10(10, 10);
 //console.log(resultado);
 //console.table(resultado);
 
@@ -68,36 +61,29 @@ let resultado = matrix10(10,10)
 // debe dar 505
 
 const sumaDiagonal = (matrix) => {
-    let acc = 0
-    for (let i = 0; i < matrix.length; i++) {
-            for (let j = 0; j < matrix[i].length; j++) {
-                if(matrix[i] === matrix[j]){
-                    acc += matrix[i][j]
-                }                
-            }    
-    }
-    return acc
-}
+  let acc = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    acc += matrix[i][i];
+  }
+
+  return acc;
+};
 
 console.log("---------------------------");
-let resultadoSumaDiagonal = sumaDiagonal(matrix10(10,10))
-console.log(resultadoSumaDiagonal)
+let resultadoSumaDiagonal = sumaDiagonal(matrix10(10, 10));
+console.log(resultadoSumaDiagonal);
 console.log("---------------------------");
 
 const sumaDiagonalInversa = (matrix) => {
-    let acc = 0
-    for (let i = matrix.length-1; i > 0 ; i--) {
-            for (let j = matrix[i].length; j > 0; j++) {
-                if(matrix[i] === matrix[j]){
-                    acc += matrix[i][j]
-                }                
-            }    
-    }
-    return acc
-}
+  let acc = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    acc += matrix[i][matrix.length - 1 - i];
+  }
+  return acc;
+};
 
-let resultadoSumaDiagonalInversa = sumaDiagonalInversa(matrix10(10,10))
-console.log(resultadoSumaDiagonalInversa)
+let resultadoSumaDiagonalInversa = sumaDiagonalInversa(matrix10(10, 10));
+console.log(resultadoSumaDiagonalInversa);
 
 /**
  * Suma los valores que sean mayores
@@ -105,9 +91,20 @@ console.log(resultadoSumaDiagonalInversa)
  */
 
 let numeros = [
-	[10, 3, 2, 1, 4, 7],
-	[5, 5, 10, 100, 4],
-	[5, 125, 10, 1020, 4],
-	[5, 5, 5097, 100, 4]
+  [10, 3, 2, 1, 4, 7],
+  [5, 5, 10, 100, 4],
+  [5, 125, 10, 1020, 4],
+  [5, 5, 5097, 100, 4],
 ];
 
+let acc = 0;
+
+for (let i = 0; i < numeros.length; i++) {
+  for (let j = 0; j < numeros[i].length; j++) {
+    if (numeros[i][j] >= 10 && numeros[i][j] < 1000) {
+      acc += numeros[i][j];
+    }
+  }
+}
+
+console.log(acc);
